00722fd95a39f72cd99008c29ec6bc83c796548d
johnjoemcbob <mcormack1994@hotmail.co.uk>
2016-01-30 14:18:33 +0000

    Camera splitscreen

---
+++ Assets/PotionCommontion/Scripts/CameraSetupScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Matthew Cormack
+// 30th - 13:54
+// Setup the camera for each player
+
+public class CameraSetupScript : MonoBehaviour
+{
+	public int Players = 1;
+	public GameObject CameraPrefab;
+
+	ArrayList Cameras = new ArrayList();
+
+	void Start()
+	{
+		Setup();
+	}
+
+	public void Setup()
+	{
+		GameObject camera;
+
+		// Instantiate a camera for each player
+		for ( int player = 0; player < Players; player++ )
+		{
+			camera = Instantiate( CameraPrefab );
+			camera.GetComponent<PlayerCameraFollowScript>().Player = GameObject.Find( "Player " + ( player + 1 ) );
+			camera.transform.parent = GameObject.Find( "Cameras" ).transform;
+			Cameras.Add( camera );
+		}
+
+		// Cameras will need to be moved
+		if ( Players != 1 )
+		{
+			// Vertical splitscreen
+			if ( Players == 2 )
+			{
+				camera = (GameObject) Cameras.ToArray()[0];
+				camera.GetComponentInChildren<Camera>().rect = new Rect( 0, 0.5f, 1, 0.5f );
+				camera = (GameObject) Cameras.ToArray()[1];
+				camera.GetComponentInChildren<Camera>().rect = new Rect( 0, 0, 1, 0.5f );
+			}
+			else if ( Players > 2 )
+			{
+				camera = (GameObject) Cameras.ToArray()[0];
+				camera.GetComponentInChildren<Camera>().rect = new Rect( 0, 0.5f, 0.5f, 0.5f );
+				camera = (GameObject) Cameras.ToArray()[1];
+				camera.GetComponentInChildren<Camera>().rect = new Rect( 0.5f, 0.5f, 0.5f, 0.5f );
+				camera = (GameObject) Cameras.ToArray()[2];
+				camera.GetComponentInChildren<Camera>().rect = new Rect( 0, 0, 0.5f, 0.5f );
+				if ( Players == 4 )
+				{
+					camera = (GameObject) Cameras.ToArray()[3];
+					camera.GetComponentInChildren<Camera>().rect = new Rect( 0.5f, 0, 0.5f, 0.5f );
+				}
+			}
+		}
+	}
+}
