63b62d82cfb0a856a8d89659e2fd7fb76b36ee7e
johnjoemcbob <mcormack1994@hotmail.co.uk>
2016-01-30 20:56:14 +0000

    Multiplayer inventory

--- Assets/Prefabs/Player/PlayerCameraFollowScript.cs
+++ Assets/Prefabs/Player/PlayerCameraFollowScript.cs
@@ -8,10 +8,29 @@ using System.Collections;
 public class PlayerCameraFollowScript : MonoBehaviour
 {
 	public float Speed = 5;
+	public float Height = 0;
+	public float Angle = 0;
 	public GameObject Player;
 
 	void Update()
 	{
-		transform.position -= ( transform.position - ( Player.transform.position + Player.transform.forward ) ) * Time.deltaTime * Speed;
+		Vector3 camforward = transform.GetChild( 0 ).forward;
+		Vector3 targetpos = ( Player.transform.position+ ( camforward * Height ) );
+		if ( Height == 0 )
+		{
+			targetpos += Player.transform.forward;
+		}
+		float distance = Vector3.Distance( targetpos, transform.position );
+		if ( distance >= 1 )
+		{
+			transform.position -= ( transform.position - targetpos ) * Time.deltaTime * Speed;
+		}
+		if (
+			( ( Angle != 0 ) && ( distance < 1 ) ) ||
+			( Angle == 0 )
+		)
+		{
+			transform.GetChild( 0 ).localEulerAngles = new Vector3( transform.GetChild( 0 ).localEulerAngles.x - ( ( transform.GetChild( 0 ).localEulerAngles.x - ( 50 + Angle ) ) * Time.deltaTime * Speed ), 0, 0 );
+		}
 	}
 }
