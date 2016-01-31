0719755976b7dfcf9ef95f5af2c9dff7a7a3a50d
johnjoemcbob <mcormack1994@hotmail.co.uk>
2016-01-31 00:15:44 +0000

    Throwing tweaks

---
+++ Assets/PotionCommontion/Scripts/GravityAlterScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Matthew Cormack
+// 30th - 23:48
+// Item gravity alterations
+
+public class GravityAlterScript : MonoBehaviour
+{
+    public float Multiplier = 1;
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().AddForce( Physics.gravity * GetComponent<Rigidbody>().mass * Multiplier );
+    }
+}
