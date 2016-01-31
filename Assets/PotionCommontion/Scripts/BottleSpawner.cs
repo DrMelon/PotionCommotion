aad19ee34cf5131e6c26eed90ffbafa900ff6901
GLITCHGEIST <caspar.vee@gmail.com>
2016-01-31 13:58:45 +0000

    Added Player Health, Fixed bottle spawner
    Bottle spawner won't try and rotate an object that isn't there anymore.
    Player health can be increased or decreased and the UI will react.

--- Assets/PotionCommontion/Scripts/BottleSpawner.cs
+++ Assets/PotionCommontion/Scripts/BottleSpawner.cs
@@ -22,8 +22,11 @@ public class BottleSpawner : MonoBehaviour {
         }
 
         //Rotate in place
-        this.transform.GetChild(0).gameObject.transform.localEulerAngles = new Vector3(this.transform.GetChild(0).gameObject.transform.localEulerAngles.x, this.transform.GetChild(0).gameObject.transform.localEulerAngles.y + (20.0f * Time.deltaTime), this.transform.GetChild(0).gameObject.transform.localEulerAngles.z);
-	}
+        if (this.transform.childCount > 0)
+        {
+            this.transform.GetChild(0).gameObject.transform.localEulerAngles = new Vector3(this.transform.GetChild(0).gameObject.transform.localEulerAngles.x, this.transform.GetChild(0).gameObject.transform.localEulerAngles.y + (20.0f * Time.deltaTime), this.transform.GetChild(0).gameObject.transform.localEulerAngles.z);
+        }
+    }
 
     void InstantiateBottle()
     {
---
+++ Assets/Prefabs/Player/HealthRespawn.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthRespawn : MonoBehaviour {
+
+    public int playerHealth;
+    private GameObject playerCanvas;
+    private Image[] heartImages;
+
+	// Use this for initialization
+	void Start () {
+
+        playerCanvas = this.transform.Find("Canvas").gameObject;
+        heartImages = playerCanvas.GetComponentsInChildren<Image>();
+
+        //3-strike system
+        playerHealth = 3;
+
+        foreach (Image heart in heartImages)
+        {
+            heart.enabled = false;
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        foreach (Image heart in heartImages)
+        {
+            heart.enabled = false;
+        }
+
+        for (int i = 0; i < playerHealth; i++)
+        {
+            heartImages[i].enabled = true;
+        }
+	
+	}
+}
